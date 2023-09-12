70e7154166ac964906cded0c2fdd536e9d0b98bd
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-09-11 22:12:59 -0500

    feat (Database): :sparkles: Conexion MongoDB
    Creacion de la Conexion a Mongo

---
+++ src/Database/connection.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+import { MONGO_URL } from "../Config/config.js";
+
+export default async function () {
+  try {
+    const client = new MongoClient(MONGO_URL.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    await client.connect();
+
+    console.log("Conexion a MongoDB exitosa");
+    const db = client.db();
+    return db;
+  } catch (error) {
+    throw new Error({ message: "Error al conectar a MongoDB:", error });
+  }
+}
